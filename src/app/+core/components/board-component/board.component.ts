1488d81762a560d6644934df1f5dacb7b032dc9f
dliszka <ddewid@gmail.com>
2018-01-13 02:44:34 +0100

    Added declaration of variables, functions on board component.

--- src/app/+core/components/board-component/board.component.ts
+++ src/app/+core/components/board-component/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'board-component',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  public user1Moves: any[] = [];
+  public user2Moves: any[] = [];
+
+  @Output()
+  public onGameEnd: EventEmitter<any> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public setListeners(): void {
+  }
+
+  public makeMove(): void {
+  }
+
+  public onMove(): void {
+  }
+
+  public renderMove(): void {
+  }
+
+  public canMove(): void {
+  }
+
+  public setGameState(): void {
   }
 
 }
