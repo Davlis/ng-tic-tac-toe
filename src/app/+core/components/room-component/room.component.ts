2b5ab9ab63b6151c71d6eaa0efb2f1abf29a7175
dliszka <ddewid@gmail.com>
2018-01-13 02:24:42 +0100

    Added function, variable declaratios to room component.

--- src/app/+core/components/room-component/room.component.ts
+++ src/app/+core/components/room-component/room.component.ts
@@ -7,12 +7,41 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class RoomComponent implements OnInit {
 
+  public users: any[] = [];
+
   @Output()
   public onLeave: EventEmitter<any> = new EventEmitter();
+
+  @Output()
+  public onStart: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public setListeners(): void {
+  }
+
+  public onRoomJoined(): void {
+  }
+
+  public onRoomLeave(): void {
+  }
+
+  public startGame(): void {
+  }
+
+  public kickUser(): void {
+  }
+
+  public addUser(): void {
+  }
+
+  public popUser(): void {
+  }
+
+  public getInviteLink(): void {
   }
 
   public leave(): void {
